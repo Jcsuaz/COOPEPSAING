77f2f83d5197fd0286b0d6611d606a5606606890
Jcsuaz <87342796+Jcsuaz@users.noreply.github.com>
2025-03-04 12:47:35 -0600

    Create admin.js

---
+++ modules/admin.js
@@ -0,0 +1,8 @@
+// Funciones para la administración
+function manageUsers() {
+  // Obtener y gestionar usuarios desde Firestore
+}
+
+function manageLoans() {
+  // Obtener y gestionar préstamos desde Firestore
+}
