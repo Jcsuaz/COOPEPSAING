1018d84e22d2f4e42d813f3a9c32645a23dcc9b1
Jcsuaz <87342796+Jcsuaz@users.noreply.github.com>
2025-03-04 15:16:03 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,4 @@
 import { auth, db } from './firebase-config.js';
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 
 export function registerUser(email, password) {
   return auth.createUserWithEmailAndPassword(email, password)
