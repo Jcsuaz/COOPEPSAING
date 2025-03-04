0d0cf5e0f50a786fe6b2d6142220b0e0ddbe169c
Jcsuaz <87342796+Jcsuaz@users.noreply.github.com>
2025-03-04 12:46:45 -0600

    Create savings.js

---
+++ modules/savings.js
@@ -0,0 +1,8 @@
+// Funciones para gestionar ahorros
+function deposit(userId, amount) {
+  // Actualizar saldo en Firestore
+}
+
+function withdraw(userId, amount) {
+  // Actualizar saldo en Firestore
+}
