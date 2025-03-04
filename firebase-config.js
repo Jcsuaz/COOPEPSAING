b111fd1cece250ac401234f3603162fe2fba86b6
Jcsuaz <87342796+Jcsuaz@users.noreply.github.com>
2025-03-04 12:42:19 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,9 +4,9 @@ const firebaseConfig = {
   projectId: "coopepsaing",
   storageBucket: "coopepsaing.firebasestorage.app",
   messagingSenderId: "412483439859",
-  appId: "1:412483439859:web:acfe406607e8015d3e7b86"
+  appId: "1:412483439859:web:acfe406607e8015d3e7b86",
 };
-// Inicializa Firebase
+
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
