2a8dbfb692ef692e87da435dd4822f39d54d6706
Jcsuaz <87342796+Jcsuaz@users.noreply.github.com>
2025-03-04 15:06:57 -0600

    Update communication.js

--- communication.js
+++ communication.js
@@ -1,4 +1,7 @@
 // Funciones para gestionar anuncios
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 function createAnnouncement(announcementData) {
   return db.collection("announcements").add(announcementData);
 }
